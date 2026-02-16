13bb2cbd97af674cdb38cb5731e06f06e4664e53
Kogar <dilloco.neocities.org@proton.me>
2026-02-16 17:49:27 +0000

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,28 @@
+/*
+  Navbar.js
+  Copyright 2026 Jackalope (dilloco)
+*/
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".nav");
+
+  if (!nav) {
+    console.warn("No .nav element");
+    return;
+  }
+
+  const links = [
+    { href: "https://dilloco.neocities.org/", text: "Home" },
+    { href: "https://dilloco.neocities.org/blog/", text: "Blog" }
+  ];
+
+  links.forEach(linkData => {
+    const a = document.createElement("a");
+    a.href = linkData.href;
+    a.textContent = linkData.text;
+    nav.appendChild(a);
+  });
+});
+
+document.querySelector("header").innerHTML = `<style>
+@import url('https://fonts.googleapis.com/css2?family=Special+Gothic+Condensed+One&display=swap');
+</style><h1 style="font-family: 'Special Gothic Condensed One';display: inline-block;" class="logo">kogar</h1><img src="https://dilloco.neocities.org/ba.png" style="height: 40px; display: inline-block; transform: scale(2)"><small style="font-family: 'Special Gothic Condensed One';display: inline-block;">knows this website is horrible</small>`;
