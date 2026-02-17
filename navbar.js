06104a429851e8d5d36e231d3b8a5d563010be00
Kogar <dilloco.neocities.org@proton.me>
2026-02-17 15:52:32 +0000

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 /*
   Navbar.js
-  Copyright 2026 Jackalope (dilloco)
+  Copyright 2026 Kogar (dilloco)
 */
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector(".nav");
@@ -25,4 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.querySelector("header").innerHTML = `<style>
 @import url('https://fonts.googleapis.com/css2?family=Special+Gothic+Condensed+One&display=swap');
-</style><h1 style="font-family: 'Special Gothic Condensed One';display: inline-block;" class="logo">kogar</h1><img src="https://dilloco.neocities.org/ba.png" style="height: 40px; display: inline-block; transform: scale(2)"><small style="font-family: 'Special Gothic Condensed One';display: inline-block;">knows this website is horrible</small>`;
+</style><h1 style="font-family: 'Special Gothic Condensed One';display: inline-block;" class="logo">kogar</h1><img src="https://dilloco.neocities.org/ba.png" style="height: 40px; display: inline-block; transform: scale(2)"><small style="font-family: 'Special Gothic Condensed One';display: inline-block;" id="sentence">knows this website is horrible</small>`;
